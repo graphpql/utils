94f0d4aebc1881a178ec783a4270a232195a3620
Václav Pelíšek <peldax@gmail.com>
2021-04-20 09:44:49 +0000

    Added reindex method to reset numeric indexes

--- src/Utils/ObjectSet.php
+++ src/Utils/ObjectSet.php
@@ -17,6 +17,13 @@ abstract class ObjectSet extends \Infinityloop\Utils\BaseSet
     {
         return \key($this->array);
     }
+    
+    public function reindex() : static
+    {
+        $this->array = \array_values($this->array);
+        
+        return $this;
+    }
 
     protected function mergeImpl(BaseSet $objectSet, bool $allowReplace = false) : static
     {
